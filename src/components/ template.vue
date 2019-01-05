f6cc2b9923c7b9ff895cb1029640fc2f2fa0a89f
ShinyaM <s.maeda.make@gmail.com>
2019-01-05 19:14:56 +0900

    template init

---
+++ src/components/ template.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="">
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'template',
+	components: {
+	},
+	props: {
+	},
+	data () {
+		return {
+		}
+	},
+	computed: {
+	},
+	methods: {
+	},
+	mounted () {
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+<style lang="scss">
+
+</style>
---
+++ src/service-worker.js
@@ -0,0 +1,18 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ */
+
+workbox.core.setCacheNameDetails({prefix: ""});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
